63f5c8f8f943dea24ecba0cd802e3d112c87f3d4
Elis Antonio Perez <codewithleader@gmail.com>
2025-04-29 12:24:07 -0500

    feat: :rocket: Añadir flujo de trabajo para desplegar contenido estático en GitHub Pages. Se configura el despliegue automático en la rama principal y se establecen los pasos necesarios para la construcción y carga del artefacto.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/app-gifs-rodrigo/',
+  base: '/app-gifs-rodrigo/', // Configuración para que funcione en github pages. More info: https://vite.dev/guide/static-deploy.html#github-pages
   plugins: [react()],
 });
